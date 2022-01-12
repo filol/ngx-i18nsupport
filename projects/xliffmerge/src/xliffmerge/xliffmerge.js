3956b2fce6555eb48a11877a416de1450da7ae41
François Dexemple <francois.dexemple@revolve.team>
2022-01-12 17:31:51 +0100

    wip

--- projects/xliffmerge/src/xliffmerge/xliffmerge.js
+++ projects/xliffmerge/src/xliffmerge/xliffmerge.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 //require('../src/xliffmerge/main.js')
-import '../src/xliffmerge/main.js';
+//import '../src/xliffmerge/main.js';
+
+let myModule;
+import("../src/xliffmerge/xliff-merge.js").then(module => {
+    myModule = module
+    myModule.main(process.argv);
+});
+
+
+// import {XliffMerge} from './xliff-merge';
+// /**
+//  * Created by martin on 15.02.2017.
+//  * Command line interface xliffmerge
+//  */
+//
+// XliffMerge.main(process.argv);
